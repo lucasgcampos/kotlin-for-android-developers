6d8910515d0a97e85b675ab3871031997a7d01b5
lucasgcampos <lucasgcampos.contato@gmail.com>
2017-01-16 21:27:31 -0200

    Start Study

---
+++ KotlinAndroid/app/src/main/java/com/lucasgcampos/kotlinandroid/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lucasgcampos.kotlinandroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+    }
+}
